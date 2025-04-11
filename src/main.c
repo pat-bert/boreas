aa5be8383917c01db3a15721c060f7ca008e0222
Patrick Bertsch <47899331+pat-bert@users.noreply.github.com>
2025-04-11 15:57:25 +0200

    add sample code to read IMU add basic KConfig settings for logging and sensor add bindings for flashing with J-Link

--- src/main.c
+++ src/main.c
@@ -7,9 +7,10 @@
 #include <stdio.h>
 #include <zephyr/kernel.h>
 #include <zephyr/drivers/gpio.h>
+#include <zephyr/drivers/sensor.h>
 
 /* 1000 msec = 1 sec */
-#define SLEEP_TIME_MS   1000
+#define SLEEP_TIME_MS 1000
 
 /* The devicetree node identifier for the "led0" alias. */
 #define LED3_NODE DT_ALIAS(led3)
@@ -20,28 +21,104 @@
  */
 static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED3_NODE, gpios);
 
+static inline float out_ev(struct sensor_value *val)
+{
+	return (val->val1 + (float)val->val2 / 1000000);
+}
+
+static void fetch_and_display(const struct device *dev)
+{
+	struct sensor_value x, y, z;
+	static int trig_cnt;
+
+	trig_cnt++;
+
+	/* lsm6dso accel */
+	sensor_sample_fetch_chan(dev, SENSOR_CHAN_ACCEL_XYZ);
+	sensor_channel_get(dev, SENSOR_CHAN_ACCEL_X, &x);
+	sensor_channel_get(dev, SENSOR_CHAN_ACCEL_Y, &y);
+	sensor_channel_get(dev, SENSOR_CHAN_ACCEL_Z, &z);
+
+	printk("accel x:%f ms/2 y:%f ms/2 z:%f ms/2\n",
+			(double)out_ev(&x), (double)out_ev(&y), (double)out_ev(&z));
+
+	/* lsm6dso gyro */
+	sensor_sample_fetch_chan(dev, SENSOR_CHAN_GYRO_XYZ);
+	sensor_channel_get(dev, SENSOR_CHAN_GYRO_X, &x);
+	sensor_channel_get(dev, SENSOR_CHAN_GYRO_Y, &y);
+	sensor_channel_get(dev, SENSOR_CHAN_GYRO_Z, &z);
+
+	printk("gyro x:%f rad/s y:%f rad/s z:%f rad/s\n",
+			(double)out_ev(&x), (double)out_ev(&y), (double)out_ev(&z));
+
+	printk("trig_cnt:%d\n\n", trig_cnt);
+}
+
+static int set_sampling_freq(const struct device *dev)
+{
+	int ret = 0;
+	struct sensor_value odr_attr;
+
+	/* set accel/gyro sampling frequency to 12.5 Hz */
+	odr_attr.val1 = 12.5;
+	odr_attr.val2 = 0;
+
+	ret = sensor_attr_set(dev, SENSOR_CHAN_ACCEL_XYZ,
+			SENSOR_ATTR_SAMPLING_FREQUENCY, &odr_attr);
+	if (ret != 0) {
+		printk("Cannot set sampling frequency for accelerometer.\n");
+		return ret;
+	}
+
+	ret = sensor_attr_set(dev, SENSOR_CHAN_GYRO_XYZ,
+			SENSOR_ATTR_SAMPLING_FREQUENCY, &odr_attr);
+	if (ret != 0) {
+		printk("Cannot set sampling frequency for gyro.\n");
+		return ret;
+	}
+
+	return 0;
+}
+
 int main(void)
 {
 	int ret;
 	bool led_state = true;
 
-	if (!gpio_is_ready_dt(&led)) {
+	const struct device *const dev = DEVICE_DT_GET_ONE(st_lsm9ds1);
+
+	if (!device_is_ready(dev))
+	{
+		printk("%s: device not ready.\n", dev->name);
+		return 0;
+	}
+
+	if (set_sampling_freq(dev) != 0) {
+		return 0;
+	}
+
+	if (!gpio_is_ready_dt(&led))
+	{
 		return 0;
 	}
 
 	ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
-	if (ret < 0) {
+	if (ret < 0)
+	{
 		return 0;
 	}
 
-	while (1) {
+	while (1)
+	{
 		ret = gpio_pin_toggle_dt(&led);
-		if (ret < 0) {
+		if (ret < 0)
+		{
 			return 0;
 		}
 
 		led_state = !led_state;
-		printf("LED state: %s\n", led_state ? "ON" : "OFF");
+		printk("LED state: %s\n", led_state ? "ON" : "OFF");
+		fetch_and_display(dev);
 		k_msleep(SLEEP_TIME_MS);
 	}
 	return 0;
